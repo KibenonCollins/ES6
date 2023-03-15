2aa22ed4fad8d646f1efd85e61ff6ceaf92d1b1d
collinskibenon <collinskibz53@gmail.com>
2023-03-15 11:58:38 +0300

    booleans2

--- app.js
+++ app.js
@@ -2,4 +2,19 @@
 // console.log ("Chris".charAt(4));
 
 //Booleans
-console.log(true);
+// var shouldBeTrue = false;
+// if (shouldBeTrue === true){
+//     console.log("This var is true!");
+// }
+// else {
+//     console.log("This var is not true")
+// };
+
+//The following evaluates to true
+var hasContent = "jjdjdxn"
+console.log(Boolean(hasContent));
+
+//the following evaluates to false
+var doesnotHaveContent = "";
+var isZero =0 ;
+console.log(Boolean(isZero));
